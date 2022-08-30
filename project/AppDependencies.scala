61e6cedbaa33c9994101ecb78fc669747f5b6630
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2022-08-30 08:41:58 +0100

    CTCP-749: Updated dependencies.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,18 +1,18 @@
-import play.core.PlayVersion
-import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
 
-  val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.24.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.68.0"
+  private val mongoVersion = "0.71.0"
+  private val bootstrapVersion = "7.1.0"
+
+  val compile: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % bootstrapVersion,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % mongoVersion
   )
 
-  val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "5.24.0"             % "test, it",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.63.0"            % Test,
-    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.36.8"            % "test, it"
-  )
+  val test: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % bootstrapVersion,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % mongoVersion,
+    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.36.8"
+  ).map(_ % "test, it")
 }
