441898e277d6793feef5d6104b24e5767dcb26b5
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2023-09-01 11:26:44 +0100

    Updated XSD.

--- app/api/submission/Header.scala
+++ app/api/submission/Header.scala
@@ -16,34 +16,29 @@
 
 package api.submission
 
-import generated.{CORRELATION_IDENTIFIERSequence, MESSAGE_1Sequence, MESSAGE_FROM_TRADERSequence, MessageType015}
+import generated.{CC015C, CORRELATION_IDENTIFIERSequence, MESSAGESequence, MESSAGE_1Sequence, MESSAGE_TYPESequence}
 import models.UserAnswers
 import play.api.libs.json.JsSuccess
 
 import java.time.LocalDateTime
-import scala.xml.NamespaceBinding
 
 object Header extends {
 
-  val scope: NamespaceBinding = scalaxb.toScope(Some("ncts") -> "http://ncts.dgtaxud.ec")
-
-  def message(uA: UserAnswers): MESSAGE_FROM_TRADERSequence =
+  def message(uA: UserAnswers): MESSAGESequence =
     uA.metadata.data.validate((preTaskListPath \ "officeOfDeparture" \ "id").read[String].map(_.take(2))) match {
       case JsSuccess(officeOfDepartureCountryCode, _) =>
-        MESSAGE_FROM_TRADERSequence(
-          messageSender = Some("NCTS"),
+        MESSAGESequence(
+          messageSender = uA.eoriNumber,
           messagE_1Sequence2 = MESSAGE_1Sequence(
             messageRecipient = s"NTA.$officeOfDepartureCountryCode",
             preparationDateAndTime = LocalDateTime.now(),
             messageIdentification = "CC015C" // TODO - check this with API team? What should this be set to?
+          ),
+          messagE_TYPESequence3 = MESSAGE_TYPESequence(CC015C),
+          correlatioN_IDENTIFIERSequence4 = CORRELATION_IDENTIFIERSequence(
+            correlationIdentifier = None // TODO - What should this be?
           )
         )
       case _ => throw new Exception("Json did not contain office of departure ID")
     }
-
-  def messageType: MessageType015 = MessageType015.fromString("CC015C", scope)
-
-  // TODO - What should this be?
-  def correlationIdentifier = CORRELATION_IDENTIFIERSequence(None)
-
 }
--- test/api/submission/ConsignmentSpec.scala
+++ test/api/submission/ConsignmentSpec.scala
@@ -14,9 +14,8 @@
  * limitations under the License.
  */
 
-package submission
+package api.submission
 
-import api.submission._
 import api.submission.consignmentType20.{activeBorderTransportMeansReads, transportEquipmentReads}
 import base.SpecBase
 import generated._
@@ -795,7 +794,7 @@ class ConsignmentSpec extends SpecBase {
         )
 
         converted.AdditionalReference shouldBe Seq(
-          AdditionalReferenceType06(
+          AdditionalReferenceType05(
             sequenceNumber = "1",
             typeValue = "ar1",
             referenceNumber = Some("arno1")
@@ -874,7 +873,7 @@ class ConsignmentSpec extends SpecBase {
                   identificationNumber = "itemSCA4"
                 )
               ),
-              Commodity = CommodityType06(
+              Commodity = CommodityType07(
                 descriptionOfGoods = "Description 1",
                 cusCode = Some("CUS code 1"),
                 CommodityCode = Some(
@@ -962,12 +961,12 @@ class ConsignmentSpec extends SpecBase {
                 )
               ),
               AdditionalReference = Seq(
-                AdditionalReferenceType05(
+                AdditionalReferenceType04(
                   sequenceNumber = "1",
                   typeValue = "ar1",
                   referenceNumber = Some("arno1")
                 ),
-                AdditionalReferenceType05(
+                AdditionalReferenceType04(
                   sequenceNumber = "2",
                   typeValue = "ar2",
                   referenceNumber = None
@@ -1000,7 +999,7 @@ class ConsignmentSpec extends SpecBase {
               referenceNumberUCR = Some("UCR 2"),
               Consignee = None,
               AdditionalSupplyChainActor = Nil,
-              Commodity = CommodityType06(
+              Commodity = CommodityType07(
                 descriptionOfGoods = "Description 2",
                 cusCode = Some("CUS code 2"),
                 CommodityCode = Some(
@@ -1032,7 +1031,7 @@ class ConsignmentSpec extends SpecBase {
               SupportingDocument = Nil,
               TransportDocument = Nil,
               AdditionalReference = Seq(
-                AdditionalReferenceType05(
+                AdditionalReferenceType04(
                   sequenceNumber = "1",
                   typeValue = "ar1",
                   referenceNumber = Some("arno1")
@@ -1513,7 +1512,7 @@ class ConsignmentSpec extends SpecBase {
       }
     }
 
-    "commodityType06 reads is called" when {
+    "commodityType07 reads is called" when {
       "commodity code defined" in {
         val json = Json.parse("""
             |{
@@ -1523,7 +1522,7 @@ class ConsignmentSpec extends SpecBase {
             |}
             |""".stripMargin)
 
-        val result = json.as[CommodityType06](commodityType06.reads)
+        val result = json.as[CommodityType07](commodityType07.reads)
 
         result.CommodityCode.value shouldBe CommodityCodeType02(
           harmonizedSystemSubHeadingCode = "commodity code",
@@ -1538,7 +1537,7 @@ class ConsignmentSpec extends SpecBase {
             |}
             |""".stripMargin)
 
-        val result = json.as[CommodityType06](commodityType06.reads)
+        val result = json.as[CommodityType07](commodityType07.reads)
 
         result.CommodityCode shouldBe None
       }
