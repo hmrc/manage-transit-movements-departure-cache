f90be5fb875f2d98cfda90a89b98cd6c9f3eb0d2
joecefnhedog <joecefnhedog@gmail.com>
2023-10-04 17:19:56 +0100

    CTCP-3765: drying the userAnswersSpec

--- test/models/UserAnswersSpec.scala
+++ test/models/UserAnswersSpec.scala
@@ -28,9 +28,9 @@ class UserAnswersSpec extends SpecBase with ScalaCheckPropertyChecks with Genera
 
   private val userAnswers = UserAnswers(
     metadata = Metadata(
-      lrn = lrn,
+      lrn        = lrn,
       eoriNumber = eoriNumber,
-      data = Json.obj(),
+      data       = Json.obj(),
       tasks = Map(
         "task1" -> Status.Completed,
         "task2" -> Status.InProgress,
@@ -38,10 +38,10 @@ class UserAnswersSpec extends SpecBase with ScalaCheckPropertyChecks with Genera
         "task4" -> Status.CannotStartYet
       )
     ),
-    createdAt = Instant.ofEpochMilli(1662393524188L),
+    createdAt   = Instant.ofEpochMilli(1662393524188L),
     lastUpdated = Instant.ofEpochMilli(1662546803472L),
-    id = UUID.fromString(uuid),
-    status = SubmissionState.NotSubmitted
+    id          = UUID.fromString(uuid),
+    status      = SubmissionState.NotSubmitted
   )
 
   "User answers" when {
@@ -71,27 +71,8 @@ class UserAnswersSpec extends SpecBase with ScalaCheckPropertyChecks with Genera
         result shouldBe userAnswers
       }
 
-      "read correctly with departureId" in {
-        val depId1        = "32r43refwd"
-        val json: JsValue = Json.parse(s"""
-             |{
-             |    "_id" : "$uuid",
-             |    "lrn" : "$lrn",
-             |    "eoriNumber" : "$eoriNumber",
-             |    "data" : {},
-             |    "tasks" : {
-             |        "task1" : "completed",
-             |        "task2" : "in-progress",
-             |        "task3" : "not-started",
-             |        "task4" : "cannot-start-yet"
-             |    },
-             |    "createdAt" : "2022-09-05T15:58:44.188Z",
-             |    "lastUpdated" : "2022-09-07T10:33:23.472Z",
-             |    "isSubmitted" : "notSubmitted",
-             |    "departureId" : "$depId1"
-             |}
-             |""".stripMargin)
-        val result        = json.as[UserAnswers]
+      "read correctly with departureId" in new DepartureIdScope {
+        val result = json.as[UserAnswers]
         result shouldBe userAnswers.copy(metadata = userAnswers.metadata.copy(departureId = Some(depId1)))
       }
 
@@ -100,27 +81,8 @@ class UserAnswersSpec extends SpecBase with ScalaCheckPropertyChecks with Genera
         result shouldBe json
       }
 
-      "write correctly with departureId" in {
-        val depId1        = "34fdsa342rd"
-        val json: JsValue = Json.parse(s"""
-             |{
-             |    "_id" : "$uuid",
-             |    "lrn" : "$lrn",
-             |    "eoriNumber" : "$eoriNumber",
-             |    "data" : {},
-             |    "tasks" : {
-             |        "task1" : "completed",
-             |        "task2" : "in-progress",
-             |        "task3" : "not-started",
-             |        "task4" : "cannot-start-yet"
-             |    },
-             |    "createdAt" : "2022-09-05T15:58:44.188Z",
-             |    "lastUpdated" : "2022-09-07T10:33:23.472Z",
-             |    "isSubmitted" : "notSubmitted",
-             |    "departureId": "$depId1"
-             |}
-             |""".stripMargin)
-        val result        = Json.toJson(userAnswers.copy(metadata = userAnswers.metadata.copy(departureId = Some(depId1))))
+      "write correctly with departureId" in new DepartureIdScope {
+        val result = Json.toJson(userAnswers.copy(metadata = userAnswers.metadata.copy(departureId = Some(depId1))))
         result shouldBe json
       }
 
@@ -164,7 +126,7 @@ class UserAnswersSpec extends SpecBase with ScalaCheckPropertyChecks with Genera
         result shouldBe userAnswers
       }
 
-      "read correctly with departureId" in new DepartureIdJsonScope {
+      "read correctly with departureId" in new DepartureIdScope {
         val result = json.as[UserAnswers](UserAnswers.mongoFormat)
         result shouldBe userAnswers.copy(metadata = userAnswers.metadata.copy(departureId = Some(depId1)))
       }
@@ -174,7 +136,7 @@ class UserAnswersSpec extends SpecBase with ScalaCheckPropertyChecks with Genera
         result shouldBe json
       }
 
-      "write correctly with departureId" in new DepartureIdJsonScope {
+      "write correctly with departureId" in new DepartureIdScope {
         val result = Json.toJson(userAnswers.copy(metadata = userAnswers.metadata.copy(departureId = Some(depId1))))((UserAnswers.mongoFormat))
         result shouldBe json
       }
@@ -183,7 +145,7 @@ class UserAnswersSpec extends SpecBase with ScalaCheckPropertyChecks with Genera
 
 }
 
-trait DepartureIdJsonScope {
+trait DepartureIdScope {
 
   val lrn           = "lrn"
   val eoriNumber    = "eori"
