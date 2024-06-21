5bb146a1ee3ce5b6c9476a953ecb5041ead7714d
Reece-Carruthers <99188015+Reece-Carruthers@users.noreply.github.com>
2024-06-21 08:34:16 +0100

    Update dependancies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,26 +2,26 @@ import sbt._
 
 object AppDependencies {
 
-  private val mongoVersion = "1.6.0"
-  private val bootstrapVersion = "8.3.0"
+  private val mongoVersion = "2.1.0"
+  private val bootstrapVersion = "9.0.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"         % mongoVersion,
     "org.typelevel"           %% "cats-core"                  % "2.9.0",
     "com.github.dwickern"     %% "scala-nameof"               % "4.0.0" % "provided",
-    "uk.gov.hmrc"             %% "crypto-json-play-30"        % "7.6.0",
+    "uk.gov.hmrc"             %% "crypto-json-play-30"        % "8.0.0",
     "javax.xml.bind"           % "jaxb-api"                   % "2.3.1"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % mongoVersion,
-    "org.scalatest"           %% "scalatest"                  % "3.2.15",
-    "org.mockito"              % "mockito-core"               % "5.2.0",
+    "org.scalatest"           %% "scalatest"                  % "3.2.18",
+    "org.mockito"              % "mockito-core"               % "5.11.0",
     "org.scalatestplus"       %% "mockito-4-6"                % "3.2.15.0",
-    "org.scalacheck"          %% "scalacheck"                 % "1.17.0",
-    "org.scalatestplus"       %% "scalacheck-1-17"            % "3.2.15.0",
-    "com.vladsch.flexmark"     % "flexmark-all"               % "0.62.2"
+    "org.scalacheck"          %% "scalacheck"                 % "1.18.0",
+    "org.scalatestplus"       %% "scalacheck-1-17"            % "3.2.18.0",
+    "com.vladsch.flexmark"     % "flexmark-all"               % "0.64.8"
   ).map(_ % "test")
 }
