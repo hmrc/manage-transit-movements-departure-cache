7e9aeda0e89c8fb8d4422f83bf8e202548372179
kara <karahan.ozturk@digital.hmrc.gov.uk>
2025-02-20 10:22:20 +0000

    CTCP-6277 Upgrade dependencies (#160)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt._
 
 object AppDependencies {
 
-  private val mongoVersion = "2.3.0"
-  private val bootstrapVersion = "9.7.0"
+  private val mongoVersion = "2.5.0"
+  private val bootstrapVersion = "9.9.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
