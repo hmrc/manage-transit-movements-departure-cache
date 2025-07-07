d63c4ac0aa1afcdace1b525712d5679168fca962
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2025-07-07 14:52:42 +0100

    CTCP-6571: Dependency updates.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object AppDependencies {
 
   private val mongoVersion = "2.6.0"
-  private val bootstrapVersion = "9.13.0"
+  private val bootstrapVersion = "9.14.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
