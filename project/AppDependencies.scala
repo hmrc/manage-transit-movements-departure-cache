48aa94bf2f257148d99cd8c8d19b4bdad40bbb47
Okeremeta Jude Tega <31142281+justega247@users.noreply.github.com>
2025-11-19 12:39:56 +0000

    CTCP6-134: Update P6 XSDs
    - update XSDs
    - update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,14 @@ import sbt.*
 
 object AppDependencies {
 
-  private val mongoVersion = "2.6.0"
-  private val bootstrapVersion = "9.14.0"
+  private val mongoVersion = "2.7.0"
+  private val bootstrapVersion = "9.19.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"         % mongoVersion,
     "org.typelevel"           %% "cats-core"                  % "2.13.0",
-    "uk.gov.hmrc"             %% "crypto-json-play-30"        % "8.2.0",
+    "uk.gov.hmrc"             %% "crypto-json-play-30"        % "8.3.0",
     "javax.xml.bind"           % "jaxb-api"                   % "2.3.1"
   )
 
@@ -17,9 +17,9 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % mongoVersion,
     "org.scalatest"           %% "scalatest"                  % "3.2.19",
-    "org.mockito"              % "mockito-core"               % "5.18.0",
+    "org.mockito"              % "mockito-core"               % "5.20.0",
     "org.scalatestplus"       %% "mockito-4-6"                % "3.2.15.0",
-    "org.scalacheck"          %% "scalacheck"                 % "1.18.1",
+    "org.scalacheck"          %% "scalacheck"                 % "1.19.0",
     "org.scalatestplus"       %% "scalacheck-1-17"            % "3.2.18.0",
     "com.vladsch.flexmark"     % "flexmark-all"               % "0.64.8"
   ).map(_ % "test")
