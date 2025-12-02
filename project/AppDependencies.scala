be3d73fc104ef4af76d0ab0f84aa5d21f7edd35f
Okeremeta Jude Tega <31142281+justega247@users.noreply.github.com>
2025-12-02 17:08:49 +0000

    CTCP6-111: Point Service to version 3.0 dynamically
    - dynamically set version
    - update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val mongoVersion = "2.10.0"
-  private val bootstrapVersion = "9.19.0"
+  private val mongoVersion = "2.11.0"
+  private val bootstrapVersion = "10.4.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
